21f3a8fb37401a8c60af83669966820219e25b73
Sandr000o <lisandro4842@gmail.com>
2024-09-23 18:19:58 -0500

    Seguriad JDBC

--- src/main/java/com/example/Practicando1/security/WebSecurityConfig.java
+++ src/main/java/com/example/Practicando1/security/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.Practicando1.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,30 +15,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 
 //Registra beans
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private DataSource dataSource;
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails user1= User.builder()
-                        .username("user")
-                        .password("$2a$10$LjlbHyU6.cHtil.6EkjEleDSPZPmPtl2Khqeo6aHrWksCtKYq/3Ku")
-                        .roles("USER")
-                        .build();
-
-
-        UserDetails user2= User.builder().
-                username("admin").
-                password("$2a$10$LjlbHyU6.cHtil.6EkjEleDSPZPmPtl2Khqeo6aHrWksCtKYq/3Ku").
-                roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user1,user2);
+    @Autowired
+    public void configAuthenticacion(AuthenticationManagerBuilder builder) throws Exception {
+        builder.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username,password, enabled FROM users where username=?")
+                .authoritiesByUsernameQuery("SELECT username, role FROM users where username=?");
     }
 
     @Bean
